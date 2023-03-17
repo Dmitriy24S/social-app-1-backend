4a8866e6330c42951b4289942ec589ab5eca22a8
Dmitriy24S <devilins24@gmail.com>
2023-03-17 10:27:43 +0200

    fix: typo in auth middleware (add token arg.)
    * jwt.verify(token, process.env.JWT_SECRET)

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,7 @@ export const verifyToken = async (req, res, next) => {
   try {
     // grab token from frontend
     let token = req.header('Authorization')
+    console.log('auth middleware token:', token)
 
     if (!token) {
       return res.status(403).send('Access Denied')
@@ -15,10 +16,14 @@ export const verifyToken = async (req, res, next) => {
       token = token.slice(7, token.length).trimLeft()
     }
 
-    const verified = jwt.verify(token.process.env.JWT_SECRET)
+    // const verified = jwt.verify(token.process.env.JWT_SECRET)
+    // const verified = jwt.verify(process.env.JWT_SECRET)
+    const verified = jwt.verify(token, process.env.JWT_SECRET)
+    console.log('auth middleware verified:', verified)
     req.user = verified
     next()
   } catch (error) {
     res.status(500).json({ error: error.message })
+    console.log('auth middleware catch error', error)
   }
 }
